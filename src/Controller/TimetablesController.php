cfc2521fe8b821a8c4c2bbc0f62d3b7782edf640
Frederik Hulleman <fhulleman@gmail.com>
2020-01-20 15:01:10 +0100

    Update timetable controler to deliver full timetables per hour incl. programmed bands/stages

--- src/Controller/TimetablesController.php
+++ src/Controller/TimetablesController.php
@@ -25,36 +25,77 @@ class TimetablesController extends AppController
         $query = $this->Festivals->find('all');
         $festival = $query->firstOrFail();
 
-        $query = $this->Timetables->find('all')
-                            ->contain(['Bands', 'Festivals', 'Dates', 'Stages'])
-                            ->where(['timetables.festival_id' => $festival->id])
-                            ->order(['timetables.date_id' => 'ASC','timetables.starttime' => 'ASC']);
-        $timetables_raw = $query->all();
+        //debug($timetables_raw);
 
         //group all existing timetable entries by date & starttime 
-        $timetables_group_by_date_starttime = array();
-        foreach ($timetables_raw as $element) {
-            $timetables_group_by_date_starttime[$element->date_id][] = $element;
-        }
+        // $timetables_group_by_date_starttime = array();
+        // foreach ($timetables_raw as $key => $element) {
+        //     //$timetables_group_by_date_starttime[$element->date_id]['date'] = $element->date->date;
+        //     $timetables_group_by_date_starttime[$element->date_id][$key] = [
+        //                                                                 'starttime' => $element->starttime,
+        //                                                                 //$element
+        //                                                             ];
+        // }
+        //debug($timetables_group_by_date_starttime);
 
         //to generate the full timetable for the festival dates, retrieve the dates model and it's properties
         $this->loadModel('Dates');
         $query = $this->Dates->find('all')->where(['dates.festival_id' => $festival->id]);
         $all_festival_dates = $query->all();
 
-        $full_time_table = array();
+        //debug($timetables_group_by_date_starttime[1]);
+        //debug($timetables_group_by_date_starttime[2]);
+
+        $timetables = array();
+        $starttimes_with_bands = array();
         foreach($all_festival_dates as $date) {
+            $timetables[$date->id]['date'] = $date->date;
+            
+            $query = $this->Timetables->find('all')
+                            //->contain(['Bands', 'Festivals', 'Dates', 'Stages'])
+                            ->where(['timetables.festival_id' => $festival->id,'timetables.date_id' => $date->id])
+                            //->order(['timetables.date_id' => 'ASC','timetables.starttime' => 'ASC'])
+                            ;
+            $timetables_with_bands = $query->all()->toArray();    
+            //debug($timetables_raw);
+            
+            $starttimes_with_bands = array_column($timetables_with_bands, 'starttime');
+            //debug($starttimes_with_bands);
+            
             $i = $date->starttime;
             while($i<$date->endtime) {
-                $full_time_table[$date->id]['date'] = $date->date;
-                $full_time_table[$date->id][] = $i;
+                //$full_time_table[$date->id]['date'] = $date->date;
+                
+                // debug($i);
+                $key = array_search($i,$starttimes_with_bands);
+                if($key !== FALSE) {
+                    $timetables[$date->id][] = [
+                        'starttime' => $i,
+                        $timetables_with_bands[$key]
+                    ];
+                    //$full_time_table[$date->id][] = ['starttime' => $i,$timetables_group_by_date_starttime[$date->id][$key]];
+                    //debug("found");
+                    // debug($date);
+                    // debug($i);
+                    // debug($key);
+                    // debug($timetables_group_by_date_starttime[$date->id][$key]);
+                    // debug("end");
+                }
+                else {
+                    $timetables[$date->id][] = [
+                        'starttime' => $i
+                    ];
+                }
+
+                
+
                 $i = $i->modify("+1 hour");
             }
         }
-        debug($full_time_table);
-        
-        $this->set(compact('full_time_table')); 
+        debug($timetables);
+        $this->set(compact('timetables')); 
     }
+    
 
     /**
      * Add method
