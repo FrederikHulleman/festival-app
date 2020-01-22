a1d3f3d5a50b08aabe709dc6ee569fbbadeea6d3
Frederik Hulleman <fhulleman@gmail.com>
2020-01-22 12:00:52 +0100

    New full data export

--- data.sql
+++ data.sql
@@ -1,115 +1,338 @@
-USE festival_app;
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 22, 2020 at 11:59 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.8
 
--- users of the organization who should be able to login and manage the festival, planning, etc. 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created DATETIME,
-    modified DATETIME,
-    UNIQUE KEY (email)
-)  CHARSET=utf8mb4;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- main festival characteristics 
-CREATE TABLE festivals (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    slug VARCHAR(191) NOT NULL,
-    description TEXT,
-    created DATETIME,
-    modified DATETIME,
-    UNIQUE KEY (slug)
-)  CHARSET=utf8mb4;
+--
+-- Database: `festival_app`
+--
 
--- dates on which the festivals take place, incl. start &  end time
-CREATE TABLE dates (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    festival_id INT NOT NULL,
-    slug DATE NOT NULL,
-    starttime DATETIME NOT NULL, 
-    endtime DATETIME NOT NULL,
-    created DATETIME,
-    modified DATETIME,
-    UNIQUE KEY (slug),
-    FOREIGN KEY festival_key (festival_id) REFERENCES festivals(id)
-)  CHARSET=utf8mb4;
+-- --------------------------------------------------------
 
--- main band characteristics 
-CREATE TABLE bands (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    slug VARCHAR(191) NOT NULL,
-    description TEXT,
-    created DATETIME,
-    modified DATETIME,
-    UNIQUE KEY (slug)
-)  CHARSET=utf8mb4;
+--
+-- Table structure for table `bands`
+--
 
--- stages available on a festival 
-CREATE TABLE stages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    festival_id INT NOT NULL, 
-    name VARCHAR(255) NOT NULL,
-    slug VARCHAR(191) NOT NULL,
-    created DATETIME,
-    modified DATETIME,
-    UNIQUE KEY (slug),
-    FOREIGN KEY festival_key (festival_id) REFERENCES festivals(id)
-)  CHARSET=utf8mb4;
+CREATE TABLE `bands` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(191) NOT NULL,
+  `description` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- timetables table: links bands, festival, festival planning & stage
--- table includes start & end time
-CREATE TABLE timetables (
-    band_id INT NOT NULL,
-    festival_id INT NOT NULL, 
-    date_id INT NOT NULL,
-    stage_id INT NOT NULL,
-    starttime VARCHAR(5) NOT NULL,
-    endtime VARCHAR(5) NOT NULL,
-    created DATETIME,
-    modified DATETIME,
-    PRIMARY KEY (band_id, festival_id, date_id, stage_id),
-    FOREIGN KEY band_key(band_id) REFERENCES bands(id),
-    FOREIGN KEY festival_key(festival_id) REFERENCES festivals(id),
-    FOREIGN KEY date_key(date_id) REFERENCES dates(id),
-    FOREIGN KEY stage_key(stage_id) REFERENCES stages(id)
-) CHARSET=utf8mb4;
+--
+-- Dumping data for table `bands`
+--
 
--- main visitor characteristics 
-CREATE TABLE visitors (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    created DATETIME,
-    modified DATETIME,
-    UNIQUE KEY (email)
-)  CHARSET=utf8mb4;
+INSERT INTO `bands` (`id`, `name`, `slug`, `description`, `created`, `modified`) VALUES
+(1, 'KCO', 'kco', 'Best orchestra of the world & Amsterdam!', '2020-01-15 17:13:16', '2020-01-16 12:42:59'),
+(2, 'RPO', 'RPO', 'Best orchestra of Rotterdam', '2020-01-15 17:13:16', '2020-01-15 17:13:16'),
+(3, 'Radio Philharmonisch Orkest', 'Radio-Philharmonisch-Orkest', 'Beautiful orchestra often performs on TV and radio live recordings.', '2020-01-15 17:13:16', '2020-01-15 17:13:16');
 
--- tickets table: links visitor, fesival & dates  
--- includes confirmed boolean to store whether the email was sent
-CREATE TABLE tickets (
-    festival_id INT NOT NULL, 
-    date_id INT NOT NULL,
-    visitor_id INT NOT NULL,
-    confirmed BOOLEAN DEFAULT FALSE,
-    created DATETIME,
-    modified DATETIME,
-    PRIMARY KEY (visitor_id, festival_id, date_id),
-    FOREIGN KEY festival_key(festival_id) REFERENCES festivals(id),
-    FOREIGN KEY date_key(date_id) REFERENCES dates(id),
-    FOREIGN KEY visitor_key(visitor_id) REFERENCES visitors(id)
-) CHARSET=utf8mb4;
+-- --------------------------------------------------------
 
-INSERT INTO users (email, password, created, modified) VALUES
-('cakephp@example.com', 'secret', NOW(),NOW());
+--
+-- Table structure for table `dates`
+--
 
-INSERT INTO festivals (title, slug, description, created, modified) VALUES
-('Leidsche Rijn Mahler Festival', 'leidsche-rijn-mahler-festival', 'Come and enjoy the best & only Mahler festival in the World, in the best place to live: Leidsche Rijn!', NOW(),NOW());
+CREATE TABLE `dates` (
+  `id` int(11) NOT NULL,
+  `festival_id` int(11) NOT NULL,
+  `slug` varchar(10) NOT NULL,
+  `date` date NOT NULL,
+  `starttime` time NOT NULL DEFAULT '00:00:00',
+  `endtime` time NOT NULL DEFAULT '00:00:00',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO bands (name, slug, description, created, modified) VALUES
-('KCO', 'kco', 'Best orchestra of the world & Amsterdam!', NOW(),NOW()),
-('RPO', 'RPO', 'Best orchestra of Rotterdam', NOW(),NOW()),
-('Radio Philharmonisch Orkest', 'Radio-Philharmonisch-Orkest', 'Beautiful orchestra often performs on TV and radio live recordings.', NOW(),NOW());
+--
+-- Dumping data for table `dates`
+--
 
-INSERT INTO dates (festival_id, slug, starttime, endtime, created, modified) VALUES
-(1, '2020-07-04', '2020-07-04 14:00:00', '2020-07-04 23:00:00', NOW(),NOW()),
-(1, '2020-07-05', '2020-07-05 14:00:00', '2020-07-05 23:00:00', NOW(),NOW());
+INSERT INTO `dates` (`id`, `festival_id`, `slug`, `date`, `starttime`, `endtime`, `created`, `modified`) VALUES
+(1, 1, '2020-07-04', '2020-07-04', '14:00:00', '23:00:00', '2020-01-16 08:36:00', '2020-01-21 14:37:06'),
+(2, 1, '2020-07-05', '2020-07-05', '14:00:00', '23:00:00', '2020-01-16 08:36:00', '2020-01-16 08:36:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `festivals`
+--
+
+CREATE TABLE `festivals` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(191) NOT NULL,
+  `description` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `festivals`
+--
+
+INSERT INTO `festivals` (`id`, `title`, `slug`, `description`, `created`, `modified`) VALUES
+(1, 'Leidsche Rijn Mahler Festival', 'leidsche-rijn-mahler-festival', 'Come and enjoy the best & only Mahler festival in the World, in the best place to live: Leidsche Rijn!', '2020-01-15 17:13:16', '2020-01-21 18:51:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stages`
+--
+
+CREATE TABLE `stages` (
+  `id` int(11) NOT NULL,
+  `festival_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(191) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stages`
+--
+
+INSERT INTO `stages` (`id`, `festival_id`, `name`, `slug`, `created`, `modified`) VALUES
+(1, 1, 'Main stage', 'main-stage', '2020-01-16 12:27:32', '2020-01-16 12:27:32'),
+(2, 1, 'Cosy stage', 'Cosy-stage', '2020-01-16 13:49:39', '2020-01-16 13:49:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `festival_id` int(11) NOT NULL,
+  `date_id` int(11) NOT NULL,
+  `visitor_id` int(11) NOT NULL,
+  `confirmed` tinyint(1) DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tickets`
+--
+
+INSERT INTO `tickets` (`festival_id`, `date_id`, `visitor_id`, `confirmed`, `created`, `modified`) VALUES
+(1, 1, 17, 1, '2020-01-22 10:54:00', '2020-01-22 10:54:02'),
+(1, 2, 17, 1, '2020-01-22 10:54:00', '2020-01-22 10:54:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timetables`
+--
+
+CREATE TABLE `timetables` (
+  `band_id` int(11) NOT NULL,
+  `festival_id` int(11) NOT NULL,
+  `date_id` int(11) NOT NULL,
+  `stage_id` int(11) NOT NULL,
+  `starttime` time NOT NULL,
+  `endtime` time NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `timetables`
+--
+
+INSERT INTO `timetables` (`band_id`, `festival_id`, `date_id`, `stage_id`, `starttime`, `endtime`, `created`, `modified`) VALUES
+(1, 1, 1, 1, '15:00:00', '15:45:00', '2020-01-16 12:28:13', '2020-01-16 12:28:13'),
+(2, 1, 2, 2, '21:00:00', '21:45:00', '2020-01-19 14:49:07', '2020-01-19 14:49:07'),
+(3, 1, 1, 1, '20:00:00', '20:45:00', '2020-01-19 14:52:35', '2020-01-19 14:52:35'),
+(3, 1, 2, 2, '19:00:00', '19:45:00', '2020-01-19 14:52:35', '2020-01-19 14:52:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `created`, `modified`) VALUES
+(1, 'cakephp@example.com', 'secret', '2020-01-15 17:13:16', '2020-01-15 17:13:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitors`
+--
+
+CREATE TABLE `visitors` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `visitors`
+--
+
+INSERT INTO `visitors` (`id`, `email`, `created`, `modified`) VALUES
+(17, 'fhulleman@gmail.com', '2020-01-22 09:56:17', '2020-01-22 09:56:17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bands`
+--
+ALTER TABLE `bands`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `dates`
+--
+ALTER TABLE `dates`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`),
+  ADD KEY `festival_key` (`festival_id`);
+
+--
+-- Indexes for table `festivals`
+--
+ALTER TABLE `festivals`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `stages`
+--
+ALTER TABLE `stages`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`),
+  ADD KEY `festival_key` (`festival_id`);
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`visitor_id`,`festival_id`,`date_id`),
+  ADD KEY `festival_key` (`festival_id`),
+  ADD KEY `date_key` (`date_id`);
+
+--
+-- Indexes for table `timetables`
+--
+ALTER TABLE `timetables`
+  ADD PRIMARY KEY (`band_id`,`festival_id`,`date_id`,`stage_id`),
+  ADD KEY `festival_key` (`festival_id`),
+  ADD KEY `date_key` (`date_id`),
+  ADD KEY `stage_key` (`stage_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `visitors`
+--
+ALTER TABLE `visitors`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bands`
+--
+ALTER TABLE `bands`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `dates`
+--
+ALTER TABLE `dates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `festivals`
+--
+ALTER TABLE `festivals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `stages`
+--
+ALTER TABLE `stages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `visitors`
+--
+ALTER TABLE `visitors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `dates`
+--
+ALTER TABLE `dates`
+  ADD CONSTRAINT `dates_ibfk_1` FOREIGN KEY (`festival_id`) REFERENCES `festivals` (`id`);
+
+--
+-- Constraints for table `stages`
+--
+ALTER TABLE `stages`
+  ADD CONSTRAINT `stages_ibfk_1` FOREIGN KEY (`festival_id`) REFERENCES `festivals` (`id`);
+
+--
+-- Constraints for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`festival_id`) REFERENCES `festivals` (`id`),
+  ADD CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`date_id`) REFERENCES `dates` (`id`),
+  ADD CONSTRAINT `tickets_ibfk_3` FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`);
+
+--
+-- Constraints for table `timetables`
+--
+ALTER TABLE `timetables`
+  ADD CONSTRAINT `timetables_ibfk_1` FOREIGN KEY (`band_id`) REFERENCES `bands` (`id`),
+  ADD CONSTRAINT `timetables_ibfk_2` FOREIGN KEY (`festival_id`) REFERENCES `festivals` (`id`),
+  ADD CONSTRAINT `timetables_ibfk_3` FOREIGN KEY (`date_id`) REFERENCES `dates` (`id`),
+  ADD CONSTRAINT `timetables_ibfk_4` FOREIGN KEY (`stage_id`) REFERENCES `stages` (`id`);
